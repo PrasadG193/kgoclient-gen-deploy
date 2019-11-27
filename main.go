f66b0d7b29b9db2497f9aac0406a1e8dc5cd9d8e
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-11-27 20:59:15 +0530

    Remove vendor and main pkg
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package serve
 
 import (
 	"fmt"
@@ -10,15 +10,6 @@ import (
 
 	"github.com/PrasadG193/kgoclient-gen/pkg/generator"
 )
-
-const API_VERSION = "v1"
-const PORT = "8080"
-
-func main() {
-	log.Printf("server started accepting requests on port=%s..\n", PORT)
-	http.HandleFunc(fmt.Sprintf("/%s/convert", API_VERSION), HandleConvert)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", PORT), nil))
-}
 
 func HandleConvert(w http.ResponseWriter, r *http.Request) {
 	// Enable CORS

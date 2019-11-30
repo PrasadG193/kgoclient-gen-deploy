82b37ec1aa3b77ee8a3228ad220efeae8f112211
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-11-30 11:06:17 +0530

    Update CORS
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- main.go
+++ main.go
@@ -13,9 +13,15 @@ import (
 
 func HandleConvert(w http.ResponseWriter, r *http.Request) {
 	// Enable CORS
+        if r.Method == http.MethodOptions {
+                w.Header().Set("Access-Control-Allow-Origin", "*")
+                w.Header().Set("Access-Control-Allow-Methods", "POST")
+                w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+                w.Header().Set("Access-Control-Max-Age", "3600")
+                w.WriteHeader(http.StatusNoContent)
+                return
+        }
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
 
 	urlPQ, _ := url.ParseQuery(r.URL.RawQuery)
 	method := generator.KubeMethod(urlPQ.Get("method"))
